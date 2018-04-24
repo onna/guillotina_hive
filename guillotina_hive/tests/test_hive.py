a8e93e79ea300fe56b440b06f43f335434e33a77
vangheem <vangheem@gmail.com>
2018-04-24 10:07:01 -0400

    fix tests

--- guillotina_hive/tests/test_hive.py
+++ guillotina_hive/tests/test_hive.py
@@ -18,10 +18,10 @@ async def test_install(hive_requester_k8s):
 
 async def test_add_task_hive_non_persist(hive_requester_k8s):
     # Lets create a task from a guillotina client that is not persist
-    async with hive_requester_k8s as requester:  # noqa
+    async with hive_requester_k8s as requester:  # noqa pylint: disable=W0612
 
         hive = get_utility(IHiveClientUtility)
-        await hive.cm.cleanup_jobs(hive.ns)
+        await hive.cm.cleanup_jobs(hive.default_namespace)
         task = Task(data={
             "name": "calculate-numbers",
             "guillotina": True,
@@ -64,9 +64,9 @@ async def test_add_task_hive_non_persist(hive_requester_k8s):
 async def test_add_task_hive_function_non_persist(hive_requester_k8s):
     # Lets create a task from a guillotina client that is not
     # persist with function
-    async with hive_requester_k8s as requester:  # noqa
+    async with hive_requester_k8s as requester:  # noqa pylint: disable=W0612
         hive = get_utility(IHiveClientUtility)
-        await hive.cm.cleanup_jobs(hive.ns)
+        await hive.cm.cleanup_jobs(hive.default_namespace)
         task = Task(data={
             "name": "random-task",
             "function": "guillotina_hive.tests.tasks.calculate_numbers",
@@ -87,7 +87,7 @@ async def test_add_task_hive_function_non_persist(hive_requester_k8s):
                 log = await hive.get_task_log(task.name)
                 print('NEW LOG')
                 print(log)
-            except:
+            except Exception:
                 pass
             job = await hive.get_task_status(task.name)
         executions = await hive.get_task_executions(task.name)
@@ -103,9 +103,9 @@ async def test_add_task_hive_function_non_persist(hive_requester_k8s):
 
 async def test_add_task_nonhive_non_persist(hive_requester_k8s):
     # Lets create a task from a guillotina client that is not persist
-    async with hive_requester_k8s as requester:  # noqa
+    async with hive_requester_k8s as requester:  # noqa pylint: disable=W0612
         hive = get_utility(IHiveClientUtility)
-        await hive.cm.cleanup_jobs(hive.ns)
+        await hive.cm.cleanup_jobs(hive.default_namespace)
         task = Task(data={
             "name": "perl-task",
             "image": "perl",
@@ -122,7 +122,7 @@ async def test_add_task_nonhive_non_persist(hive_requester_k8s):
                 log = await hive.get_task_log(task.name)
                 print('NEW LOG')
                 print(log)
-            except:
+            except Exception:
                 pass
 
             job = await hive.get_task_status(task.name)
