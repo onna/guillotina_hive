85af330faf959c672e44275eee745c5dea48e89c
vangheem <vangheem@gmail.com>
2018-05-01 17:55:52 -0400

    another

--- guillotina_hive/tests/test_hive.py
+++ guillotina_hive/tests/test_hive.py
@@ -31,24 +31,24 @@ async def test_add_task_hive_non_persist(hive_requester_k8s):
         })
         await reconfigure_db(hive, task)
         await hive.run_task(task)
-        job = await hive.get_task_status(task.name)
+        job = await hive.get_task_status(task.id)
         assert isinstance(job, Job)
         while job.finished is False:
             await asyncio.sleep(10)
             try:
-                log = await hive.get_task_log(task.name)
+                log = await hive.get_task_log(task.id)
                 print('NEW LOG')
                 print(log)
             except Exception:
                 pass
 
-            job = await hive.get_task_status(task.name)
+            job = await hive.get_task_status(task.id)
 
         tries = 0
         while tries < 5:
-            executions = await hive.get_task_executions(task.name)
+            executions = await hive.get_task_executions(task.id)
             print(executions.statuses())
-            log = await hive.get_task_log(task.name)
+            log = await hive.get_task_log(task.id)
             print(log)
             try:
                 assert executions.is_done()
@@ -79,20 +79,20 @@ async def test_add_task_hive_function_non_persist(hive_requester_k8s):
         assert task.envs['PAYLOAD'] is not None
         await reconfigure_db(hive, task)
         await hive.run_task(task)
-        job = await hive.get_task_status(task.name)
+        job = await hive.get_task_status(task.id)
         assert isinstance(job, Job)
         while job.finished is False:
             await asyncio.sleep(10)
             try:
-                log = await hive.get_task_log(task.name)
+                log = await hive.get_task_log(task.id)
                 print('NEW LOG')
                 print(log)
             except Exception:
                 pass
-            job = await hive.get_task_status(task.name)
-        executions = await hive.get_task_executions(task.name)
+            job = await hive.get_task_status(task.id)
+        executions = await hive.get_task_executions(task.id)
         print(executions.statuses())
-        log = await hive.get_task_log(task.name)
+        log = await hive.get_task_log(task.id)
         print(log)
         for execution in executions:
             print('EXECUTION')
@@ -114,19 +114,19 @@ async def test_add_task_nonhive_non_persist(hive_requester_k8s):
 
         await hive.run_task(task)
 
-        job = await hive.get_task_status(task.name)
+        job = await hive.get_task_status(task.id)
         assert isinstance(job, Job)
         while job.finished is False:
             await asyncio.sleep(10)
             try:
-                log = await hive.get_task_log(task.name)
+                log = await hive.get_task_log(task.id)
                 print('NEW LOG')
                 print(log)
             except Exception:
                 pass
 
-            job = await hive.get_task_status(task.name)
-        executions = await hive.get_task_executions(task.name)
+            job = await hive.get_task_status(task.id)
+        executions = await hive.get_task_executions(task.id)
         assert executions.is_done()
-        log = await hive.get_task_log(task.name)
+        log = await hive.get_task_log(task.id)
         assert '3.14' in log
